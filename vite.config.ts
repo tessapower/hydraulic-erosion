e8802d558e0600e2a625a8b45ef88a832807c9a0
Tessa Power <hello@tessapower.xyz>
2025-11-04 11:18:59 -0600

    Enable support for glsl shaders

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
-  plugins: [],
+  plugins: [glsl()],
   base: "/hydraulic-erosion/",
+  assetsInclude: ["**/*.glsl"],
 });
