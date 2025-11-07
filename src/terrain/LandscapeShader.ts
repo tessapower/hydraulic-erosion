4340aa9c8eb0fc57524ae20b8fd5d174e64bcd7f
Tessa Power <hello@tessapower.xyz>
2025-11-06 19:24:38 -0600

    Use UniformsUtils.merge to send fog uniforms to shader

--- src/terrain/LandscapeShader.ts
+++ src/terrain/LandscapeShader.ts
@@ -4,20 +4,19 @@ import fragShader from "../shaders/terrain.fs.glsl?raw";
 
 export function createLandscapeShader(): THREE.ShaderMaterial {
   return new THREE.ShaderMaterial({
-    uniforms: {
-      // Color based on slope
-      u_flatColor: {value: new THREE.Color(0x72a172)},  // Grass green
-      u_steepColor: {value: new THREE.Color(0xcbbfb4)}, // Dirt brown
-      u_steepness: {value: 0.6},                                 // Threshold
-      // Lighting
-      u_lightDirection: {value: new THREE.Vector3(1, 1, 1).normalize()},
-      u_lightColor: {value: new THREE.Color(1.0, 1.0, 0.9)},
-      u_lightStrength: {value: 1.3},
-      // Fog: initialize with default values - will be set by SceneManager
-      fogColor: {value: new THREE.Color()},
-      fogNear: {value: 0},
-      fogFar: {value: 0},
-    },
+    uniforms: THREE.UniformsUtils.merge([
+      THREE.UniformsLib.fog, // Automatically includes fogColor, fogNear, fogFar
+      {
+        // Color based on slope
+        u_flatColor: {value: new THREE.Color(0x72a172)},  // Grass green
+        u_steepColor: {value: new THREE.Color(0xcbbfb4)}, // Dirt brown
+        u_steepness: {value: 0.6},                                 // Threshold
+        // Lighting
+        u_lightDirection: {value: new THREE.Vector3(1, 1, 1).normalize()},
+        u_lightColor: {value: new THREE.Color(1.0, 1.0, 0.9)},
+        u_lightStrength: {value: 1.3},
+      }
+    ]),
     vertexShader: vertShader,
     fragmentShader: fragShader,
     toneMapped: false,
