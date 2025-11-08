59a2eb69bf076d9f2d8f77f89985273e00b1d1d4
Tessa Power <hello@tessapower.xyz>
2025-11-08 13:14:13 -0600

    Add support for disabling sliders (in future)

--- src/gui/ShaderControls.ts
+++ src/gui/ShaderControls.ts
@@ -2,40 +2,41 @@
 
 import * as THREE from "three";
 import type {IGuiModule} from "./GuiManager";
-import type GUI from "lil-gui";
+import GUI from "lil-gui";
 
 export class ShaderControls implements IGuiModule {
   private material: THREE.ShaderMaterial;
+  private shaderFolder: GUI = null!;
 
   constructor(material: THREE.ShaderMaterial) {
     this.material = material;
   }
 
   registerParent(parentGui: GUI): void {
-    const folder = gui.addFolder("Rendering");
+    this.shaderFolder = parentGui.addFolder(this.getModuleName());
 
     const colors = {
       flatColor: this.material.uniforms.u_flatColor.value.getHex(),
       steepColor: this.material.uniforms.u_steepColor.value.getHex(),
     };
 
-    folder.addColor(colors, "flatColor").onChange((value: number) => {
+    this.shaderFolder.addColor(colors, "flatColor").onChange((value: number) => {
       this.material.uniforms.u_flatColor.value.setHex(value);
     }).name("Flat Color");
 
-    folder.addColor(colors, "steepColor").onChange((value: number) => {
+    this.shaderFolder.addColor(colors, "steepColor").onChange((value: number) => {
       this.material.uniforms.u_steepColor.value.setHex(value);
     }).name("Steep Color");
 
-    folder
+    this.shaderFolder
       .add(this.material.uniforms.u_steepness, "value", 0.0, 1.0, 0.05)
       .name("Steepness Threshold");
 
-    folder
+    this.shaderFolder
       .add(this.material.uniforms.u_lightStrength, "value", 0.0, 3.0, 0.1)
       .name("Light Strength");
 
-    folder.close();
+    this.shaderFolder.close();
   }
 
   getModuleName(): string {
